d08e53bea66432ee0e64eb74babf464324afeff4
a-chun-lit <akariakaringo.20021226@gmail.com>
2022-09-12 02:39:26 +0900

    second

--- js/main.js
+++ js/main.js
@@ -50,7 +50,7 @@ $(function($){
         return false;
     })
 
-    particlesJS("courses", {
+    particlesJS("particles-js", {
         particles: {
           number: { value: 80, density: { enable: true, value_area: 800 } },
           color: { value: "#ffffff" },
